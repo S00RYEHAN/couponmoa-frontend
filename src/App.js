369a33b6a8812d6ed42f2a7db1adfd8992060ec2
owlsleep315 <chahwang0127@naver.com>
2025-07-02 18:39:00 +0900

    tes2t

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          seongminkim <code>src/App.js</code> and save to reload.
+          owlsleep <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
